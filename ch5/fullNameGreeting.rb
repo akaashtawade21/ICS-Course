a975584c0403e8894a3aa92886f156bb7bea3f89
Akaash Tawade <akaash@Akaashs-MacBook-Pro.local>
2017-06-05 16:59:46 -0700

    New HW

---
+++ ch5/fullNameGreeting.rb
@@ -0,0 +1,8 @@
+puts 'Please enter your first name'
+firstName = gets.chomp
+puts 'Please enter your middle name'
+middleName = gets.chomp
+puts 'Please enter your last name'
+lastName = gets.chomp
+
+puts 'Hello ' + firstName + ' ' + middleName + ' ' + lastName + ', nice to meet you!'
