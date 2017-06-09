d9994f685805d6a80203d7e26a4cb2214dda781f
Akaash Tawade <akaash@Akaashs-MacBook-Pro.local>
2017-06-08 19:29:02 -0700

    Homework

---
+++ ch6/angryBoss.rb
@@ -0,0 +1,3 @@
+puts "WHAT DO YA WANT??!"
+response = gets.chomp
+puts "WHADDAYA MEAN \"" + response.upcase + "\"?!?!?! YOU\'RE FIRED!!!"
---
+++ ch6/tableOfContents.rb
@@ -0,0 +1,10 @@
+line_width = 50
+str = "Table of Contents"
+str2 = "Chapter 1: Getting Started                 page 1"
+str3 = "Chapter 2: Numbers                         page 9"
+str4 = "Chapter 3: Letters                        page 13"
+puts(str.center(line_width))
+puts
+puts(str2.ljust(line_width))
+puts(str3.ljust(line_width))
+puts(str4.ljust(line_width))
