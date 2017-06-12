f0e72a4b48bb8de28222dd9017e2c3d4bd65decb
Akaash Tawade <akaash@Akaashs-MacBook-Pro.local>
2017-06-11 17:22:41 -0700

    Updated 99 Bottles

--- ch7/99BottlesOfBeer.rb
+++ ch7/99BottlesOfBeer.rb
@@ -1,6 +1,11 @@
 num = 99
 while num > 1
-  puts num.to_s + " bottles of beer on the wall, " + num.to_s + " bottles of beer. Take one down and pass it around, " + num.to_s + " bottles of beer on the wall."
+  puts num.to_s + " bottles of beer on the wall, " + num.to_s + " bottles of beer."
   num = num - 1
+  puts "Take one down and pass it around, " + num.to_s + " bottles of beer on the wall."
 end
-puts num.to_s + " bottle of beer on the wall, " + num.to_s + " bottle of beer. Take one down and pass it around, no more bottles of beer on the wall."
+puts num.to_s + " bottle of beer on the wall, " + num.to_s + " bottle of beer."
+puts "Take one down and pass it around, no more bottles of beer on the wall."
+puts "No more bottles of beer on the wall, no more bottles of beer."
+num = 99
+puts "Go to the store and buy some more, " + num.to_s + " bottles of beer on the wall."
